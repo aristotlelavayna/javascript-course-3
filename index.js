62ebee88b80e0b7e240225ffc7edf6d00957017e
Aristotle Lavayna <aristotlelavayna@gmail.com>
2024-01-03 22:52:50 +0800

    commit

--- index.js
+++ index.js
@@ -135,3 +135,18 @@ document.getElementById('rollButton').onclick = function () {
     document.getElementById('zlabel').innerHTML = z; 
 } */
 //////////////// END OF RANDOM NUMBER GENERATOR /////////////////////////
+
+
+//////////////////////////// STRING SLICING /////////////////////////////
+// slice() extracts a section of a string and returns it as a new string without modifying th original string.
+
+/* let fullName = "aristotle lavayna";
+let firstName;
+let lastName;
+
+firstName = fullName.slice(0, fullName.indexOf(" "));
+lastName = fullName.slice(fullName.indexOf(" ") + 1);
+
+console.log(firstName);
+console.log(lastName); */
+///////////////////////// END OF STRING SLICING /////////////////////////
