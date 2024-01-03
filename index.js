d87114676a01b2d6c9b8732ae8602c7d19312e6e
Aristotle Lavayna <aristotlelavayna@gmail.com>
2024-01-03 18:00:35 +0800

    commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// USER INPUT LESSON
+// easy way with window promt
+/* let username = window.prompt("what's your name?");
+console.log(username); */
+
+// difficult way html textbox
+/* document.getElementById('myButton').onclick = function () {
+    username = document.getElementById('myText').value;
+    console.log(username);
+    document.getElementById('myLabel').innerHTML = 'hello ' + username;
+} */
+// END OF USER INPUT LESSON
+
+// TYPE CONVERSION LESSON
+/* let age = window.prompt('how old are you?');
+
+console.log(typeof age);
+age = Number(age);
+age += 1;
+
+console.log('happy birthday! you are', age, 'years old'); */
+
+/* let x;
+let y;
+let z;
+
+x = Number('3.14');
+y = String(3.14);
+z = Boolean("pizza");
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z); */
+// END OF TYPE CONVERSION LESSON
