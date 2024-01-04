f0dde646d78bac921e6a24af179bd9b0286d8974
Aristotle Lavayna <aristotlelavayna@gmail.com>
2024-01-04 22:22:07 +0800

    commit

---
+++ nested-loops/nested-loops.js
@@ -0,0 +1,12 @@
+// nested loops is a loop inside another loop.
+
+let symbol = window.prompt("Enter a symbol to use.");
+let rows = window.prompt("Enter # of rows.");
+let columns = window.prompt("Enter # columns.");
+
+for (let i = 1; i <= rows; i+=1) {
+    for (let j = 1; j <= columns; j+=1) {
+        document.getElementById("myRectangle").innerHTML += symbol;
+    }
+    document.getElementById("myRectangle").innerHTML += "<br>";
+}
