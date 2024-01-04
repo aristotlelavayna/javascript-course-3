cf0e392dd0181c831f7888ffa7993517837faa1d
Aristotle Lavayna <aristotlelavayna@gmail.com>
2024-01-04 18:20:09 +0800

    commit

--- index.js
+++ index.js
@@ -164,4 +164,25 @@ if (temperature <= 0 || temperature >= 30) {
 } else {
     console.log("The weather is bad");
 } */
-////////////////// END OF && || OPERATORS LESSON /////////////////////// 
+////////////////// END OF && || OPERATORS LESSON ///////////////////////
+
+
+////////////////////// ! NOT LOGICAL OPERATOR ///////////////////////////
+// typically used to reverse a condition's boolean value.
+// true -> false, false -> true
+
+/* let temp = 15;
+let sunny = true;
+
+if (!(temp > 0)) {
+    console.log("It's cold outside");
+} else {
+    console.log("It's warm outside");
+}
+
+if (!sunny) {
+    console.log("It's cloudy outside");
+} else {
+    console.log("It's sunny outside");
+} */
+//////////////////// END OF ! NOT LOGICAL OPERATOR //////////////////////
