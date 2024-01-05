44d6c1360172f5098b16f7ebb47ef80c710f6936
Aristotle Lavayna <aristotlelavayna@gmail.com>
2024-01-05 18:54:53 +0800

    commit

--- LESSONS/index-2.js
+++ LESSONS/index-2.js
@@ -98,3 +98,19 @@ function sum(...numbers) {
     return total
 } */
 //////////////////// END OF REST PARAMETERS LESSON //////////////////////
+
+
+/////////////////////// array.forEach() LESSON //////////////////////////
+let students = ["spongebob", "patrick", "squidward"];
+students.forEach(capitalize);
+students.forEach(print);
+
+function capitalize(element, index, array) {
+    array[index] = element[0].toUpperCase() + element.substring(1);
+}
+
+//console.log(students[0]);
+
+function print(element) {
+    console.log(element);
+}
--- callbacks/callbacks.js
+++ callbacks/callbacks.js
@@ -1,6 +1,7 @@
 // callback = a function passed as an argument to another function.
 // Ensures that a function is not going to run before a task is completed. Helps us develop asynchronous code. (When one function has to wait for another function) that helps us avoid errors and potential problems. Ex. Wait for a file to load.
-sum(2, 3, displayDOM);
+
+sum(2, 3, displayDOM); // or displayConsole
 
 function sum(x, y, callBack) {
     let result = x + y;
