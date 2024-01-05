30a87e95aade37e876f40cc8d2b64397fb63fc5e
Aristotle Lavayna <aristotlelavayna@gmail.com>
2024-01-05 18:09:25 +0800

    commit

--- LESSONS/index-2.js
+++ LESSONS/index-2.js
@@ -79,3 +79,22 @@ class1.push(...class2);
 
 console.log(...class1); */ // or console.log(class1);
 ///////////////////// END OF SPREAD OPERATOR LESSON /////////////////////
+
+
+//////////////////////// REST PARAMETERS LESSON /////////////////////////
+/* let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+
+console.log(sum(a, b, c, d, e));
+
+function sum(...numbers) {
+    let total = 0;
+    for (let number of numbers) {
+        total += number
+    }
+    return total
+} */
+//////////////////// END OF REST PARAMETERS LESSON //////////////////////
