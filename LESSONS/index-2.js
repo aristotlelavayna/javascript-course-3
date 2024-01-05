abddb177cd24f22507e0724c6e2c5b1bd4616f5d
Aristotle Lavayna <aristotlelavayna@gmail.com>
2024-01-05 20:53:01 +0800

    commit

--- LESSONS/index-2.js
+++ LESSONS/index-2.js
@@ -139,3 +139,21 @@ function print(element) {
     console.log(element);
 } */
 ////////////////////// END OF array.map() LESSON ////////////////////////
+
+
+/////////////////////// array.filter() LESSON ///////////////////////////
+// array.filter() = creates a new array with all elements that pass the test provided by a function.
+
+/* let ages = [18, 16, 21, 17, 19, 90];
+let adults = ages.filter(checkAge);
+
+adults.forEach(print);
+
+function checkAge(element) {
+    return element >= 18;
+}
+
+function print(element) {
+    console.log(element);
+} */
+//////////////////// END OF array.filter() LESSON ///////////////////////
