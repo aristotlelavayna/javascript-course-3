c57c4941859432046d59de5a6ab9212cc9614b53
Aristotle Lavayna <aristotlelavayna@gmail.com>
2024-01-05 17:24:08 +0800

    commit

--- LESSONS/index-2.js
+++ LESSONS/index-2.js
@@ -1,7 +1,7 @@
 /////////////////////////// ARRAYS LESSON ///////////////////////////////
 // array = a variable that can store multiple values.
 
-let fruits = ["apple", "orange", "banana"];
+/* let fruits = ["apple", "orange", "banana"];
 
 fruits.push("lemon"); // add an element.
 fruits.pop(); // removes last element.
@@ -11,5 +11,25 @@ fruits.shift(); // removes element from beginning.
 let length = fruits.length;
 let index = fruits.indexOf("banana");
 
-console.log(index);
+console.log(index); */
 ////////////////////////// END OF ARRAYS LESSON /////////////////////////
+
+
+////////////////////// LOOP THROUGH AN ARRAY LESSON /////////////////////
+let prices = [5, 10, 15, 20, 25];
+
+// standard for loop
+/* for (let i = 0; i < prices.length; i+=1) {
+    console.log(prices[i]);
+} */
+
+// reverse for loop
+/* for (let i = prices.length - 1; i >= 0; i-=1) {
+    console.log(prices[i]);
+} */
+
+// for up statement
+/* for (let price of prices) {
+    console.log(price);
+} */
+///////////////// END OF LOOP THROUGH AN ARRAY LESSON ////////////////// 
