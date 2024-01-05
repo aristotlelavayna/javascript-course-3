944131b083a56bca1eb5d3f8b3913fbb341721a6
Aristotle Lavayna <aristotlelavayna@gmail.com>
2024-01-05 13:53:58 +0800

    commit

---
+++ LESSONS/index-2.js
@@ -0,0 +1,15 @@
+/////////////////////////// ARRAYS LESSON ///////////////////////////////
+// array = a variable that can store multiple values.
+
+let fruits = ["apple", "orange", "banana"];
+
+fruits.push("lemon"); // add an element.
+fruits.pop(); // removes last element.
+fruits.unshift("mango"); // add element to beginning.
+fruits.shift(); // removes element from beginning.
+
+let length = fruits.length;
+let index = fruits.indexOf("apple");
+
+console.log(index);
+////////////////////////// END OF ARRAYS LESSON /////////////////////////
