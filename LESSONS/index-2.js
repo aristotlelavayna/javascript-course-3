955d6c165ab88b4b7545cbd86bf45437b3575a27
Aristotle Lavayna <aristotlelavayna@gmail.com>
2024-01-06 21:48:10 +0800

    commit

--- LESSONS/index-2.js
+++ LESSONS/index-2.js
@@ -244,3 +244,28 @@ grades.sort((x, y) => y - x);
 grades.forEach((element) => console.log(element)); */
 
 /////////////// END OF ARROW FUNCTION EXPRESSIONS LESSON ////////////////
+
+
+/////////////////////// SHUFFLE AN ARRAY LESSON /////////////////////////
+let cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+shuffle(cards);
+
+//console.log(cards[0]);
+cards.forEach(card => console.log(card));
+
+function shuffle(array) {
+    let currentIndex = array.length;
+
+    while(currentIndex != 0) {
+        let randomIndex = Math.floor(Math.random() * array.length);
+        currentIndex-=1;
+
+        let temp = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temp;
+    }
+
+    return array;
+}
+//////////////////// END OF SHUFFLE AN ARRAY LESSON /////////////////////
