3932cf5d845b44a6a9a7d699927e598baf46770e
Aristotle Lavayna <aristotlelavayna@gmail.com>
2024-01-06 23:00:43 +0800

    commit

---
+++ LESSONS/index-3.js
@@ -0,0 +1,23 @@
+/////////////////////////// OBJECTS LESSON //////////////////////////////
+// object = a group of properties and methods. 
+// properties = what an object has.
+// methods = what an object can do use to access properties/ methods.
+
+/* const car = {
+    model: "Mustang",
+    color: "red",
+    year: 2023,
+
+    drive : function () {
+        console.log("You drive the car.");
+    },
+    brake : function () {
+        console.log("You stepped on the brakes.");
+    }
+}
+
+console.log(car.model);
+
+car.drive(); */
+
+//////////////////////// END OF OBJECTS LESSON //////////////////////////
