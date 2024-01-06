b38434e1c585b41983ea46ee39f4519a093e5b63
Aristotle Lavayna <aristotlelavayna@gmail.com>
2024-01-06 22:43:47 +0800

    commit

--- LESSONS/index-2.js
+++ LESSONS/index-2.js
@@ -291,3 +291,31 @@ function login() {
     }
 } */
 /////////////////// END OF NESTED FUNNCTIONS LESSON /////////////////////
+
+
+///////////////////////////// MAPS LESSON ///////////////////////////////
+// map = object that holds key-value pairs of any data type.
+
+/* const store = new Map([
+    ["t-shirt", 20],
+    ["jeans", 30],
+    ["socks", 10],
+    ["underwear", 50]
+]);
+
+/* let shoppingCart = 0;
+
+shoppingCart += store.get("t-shirt");
+shoppingCart += store.get("underwear");
+console.log(shoppingCart); */
+
+// different kinds of methods:
+//store.get("t-shirt");
+//store.set("hat", 40);
+//store.delete("hat");
+//console.log(store.has("hat"));
+console.log(store.size);
+
+store.forEach((value, key) => console.log(`${key} $${value}`)); */
+
+///////////////////////// END OF MAPS LESSON ////////////////////////////
