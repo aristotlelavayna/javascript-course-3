db65e44384e0f91e24ed13042abe633438b4916c
Aristotle Lavayna <aristotlelavayna@gmail.com>
2024-01-07 20:31:47 +0800

    commit

--- LESSONS/index-3.js
+++ LESSONS/index-3.js
@@ -26,7 +26,7 @@ car.drive(); */
 /////////////////////////// CLASSES LESSON //////////////////////////////
 // class = a blueprint for creating objects define what properties and methods they have use a constructor for unique properties.
 
-class Player {
+/* class Player {
     score = 0;
 
     pause () {
@@ -48,5 +48,36 @@ console.log(player1.score);
 console.log(player2.score);
 
 player1.pause();
-player2.exit();
+player2.exit(); */
 /////////////////////// END OF CLASSES LESSON ///////////////////////////
+
+
+//////////////////////// CONSTRUCTOR LESSON /////////////////////////////
+// constructor = a special method of a class, accepts arguments and assigns properties.
+
+class Student {
+
+    constructor (name, age, gpa) {
+        this.name = name;
+        this.age = age;
+        this.gpa = gpa;
+    }
+    study () {
+        console.log(`${this.name} is studying`);
+    }
+}
+
+const student1 = new Student ("Spongebob", 30, 3.2);/* 
+const student2 = new Student ("Patrick", 35, 1.5);
+
+console.log(student1.name);
+console.log(student1.age);
+console.log(student1.gpa);
+student1.study();
+
+console.log(student2.name);
+console.log(student2.age);
+console.log(student2.gpa);
+student2.study(); */
+
+////////////////////// END OF CONSTRUCTORS LESSON ///////////////////////
