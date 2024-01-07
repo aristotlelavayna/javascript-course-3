0a06126f8eb41b690d1cd7f583b87873a3411e23
Aristotle Lavayna <aristotlelavayna@gmail.com>
2024-01-07 21:01:37 +0800

    commit

--- LESSONS/index-3.js
+++ LESSONS/index-3.js
@@ -106,3 +106,57 @@ console.log(Car.numberOfCars);
 Car.startRace(); */
 
 ////////////////////////// END OF STATIC LESSON /////////////////////////
+
+
+/////////////////////////// INHERITANCE LESSON //////////////////////////
+// inheritance = a child class can inherit all the methods and properties from another class.
+
+/* class Animal { // <= this is the parent class
+    alive = true;
+
+    eat () {
+        console.log(`This ${this.name} is eating.`);
+    }
+    sleep () {
+        console.log(`This ${this.name} is sleeping`);
+    }
+}
+
+// these are the children class
+class Rabbit extends Animal {
+    
+    name = "rabbit";
+
+    run () {
+        console.log(`This ${this.name} is running.`);
+    }
+}
+
+class Fish extends Animal {
+    
+    name = "fish";
+
+    swim () {
+        console.log(`This ${this.name} is swimming.`);
+    }
+}
+
+class Hawk extends Animal {
+    
+    name = "hawk";
+
+    fly () {
+        console.log(`This ${this.name} is flying.`);
+    }
+}
+
+const rabbit = new Rabbit();
+const fish = new Fish();
+const hawk = new Hawk();
+
+console.log(rabbit.alive);
+rabbit.eat();
+rabbit.sleep();
+rabbit.run(); */
+
+//////////////////////// END OF INHERITANCE LESSON //////////////////////
