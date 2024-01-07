092f4854963c75d142716e5d77e3030a7f7db43f
Aristotle Lavayna <aristotlelavayna@gmail.com>
2024-01-07 22:22:54 +0800

    commit

--- LESSONS/index-3.js
+++ LESSONS/index-3.js
@@ -159,4 +159,46 @@ rabbit.eat();
 rabbit.sleep();
 rabbit.run(); */
 
-//////////////////////// END OF INHERITANCE LESSON //////////////////////
+/////////////////////// END OF INHERITANCE LESSON ///////////////////////
+
+
+///////////////////////// SUPER KEYWORD LESSON ///////////////////////// 
+// super = refers to the parent class. commonly used to invoke constructor of a parent class.
+
+/* class Animal {
+    constructor (name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Rabbit extends Animal {
+    constructor (name, age, runSpeed) {
+        super (name, age);
+        this.runSpeed = runSpeed;
+    }
+}
+
+class Fish extends Animal {
+    constructor (name, age, swimSpeed) {
+        super (name, age);
+        this.swimSpeed = swimSpeed;
+    }
+}
+
+class Hawk extends Animal {
+    constructor (name, age, flySpeed) {
+        super (name, age);
+        this.flySpeed = flySpeed;
+    }
+}
+
+const rabbit = new Rabbit ("rabbit", 1, 40);
+const fish = new Fish ("fish", 2, 80);
+const hawk = new Hawk ("hawk", 3, 200);
+
+console.log(rabbit.name);
+console.log(rabbit.age);
+console.log(rabbit.runSpeed); */
+
+/////////////////////// END OF SUPER KEYWORD LESSON /////////////////////
