1a3f077d79990b9f109f51c97e7dd11acbc8b596
Aristotle Lavayna <aristotlelavayna@gmail.com>
2024-01-08 22:12:46 +0800

    commit

--- LESSONS/index-4.js
+++ LESSONS/index-4.js
@@ -17,3 +17,22 @@ finally {
     alert("This always executes"); // changed from console.logpip
 } */
 //////////////////// END OF ERROR HANDLING LESSON ///////////////////////
+
+
+//////////////////////// setInterval() LESSON ///////////////////////////
+// setinterval() = invokes a function repeatedly after a number of milliseconds asynchronous function (doesn't pause execution)
+
+/* let count = 0;
+let max = window.prompt("Count up to what number?");
+max = Number(max);
+
+const myTimer = setInterval(countUp, 1000);
+
+function countUp() {
+    count+=1;
+    console.log(count);
+    if (count >= max) {
+        clearInterval(myTimer);
+    }
+} */
+////////////////////// END OF setInterval LESSON ////////////////////////
