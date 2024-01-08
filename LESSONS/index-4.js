882e9367e61c2aa68ef82eddfef27dba8146448f
Aristotle Lavayna <aristotlelavayna@gmail.com>
2024-01-08 19:07:34 +0800

    commit

---
+++ LESSONS/index-4.js
@@ -0,0 +1,19 @@
+//////////////////////// ERROR HANDLING LESSON //////////////////////////
+// error = object with a description of something that went wrong.
+// throw = executes a user defined error.
+
+/* try {
+    let x = window.prompt("Enter a number");
+    x = Number(x);
+
+    if (isNaN(x)) throw alert("That wasn't a number!"); //alert was added
+    if(x == "") throw alert("That was empty!");
+
+    alert(`${x} is a number`); // changed from console.log
+} catch (error) {
+    console.log(error);
+}
+finally {
+    alert("This always executes"); // changed from console.logpip
+} */
+//////////////////// END OF ERROR HANDLING LESSON ///////////////////////
