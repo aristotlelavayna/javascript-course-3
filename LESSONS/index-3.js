18ce3d97f81971c7ffbe3547727b024262a0731f
Aristotle Lavayna <aristotlelavayna@gmail.com>
2024-01-08 17:58:27 +0800

    commit

--- LESSONS/index-3.js
+++ LESSONS/index-3.js
@@ -202,3 +202,67 @@ console.log(rabbit.age);
 console.log(rabbit.runSpeed); */
 
 /////////////////////// END OF SUPER KEYWORD LESSON /////////////////////
+
+
+////////////////////// GETTERS AND SETTERS LESSON ///////////////////////
+// get = binds an object property to a function when that property is accessed.
+// set = binds an object property to a function when that property is assigned a value.
+
+/* class Car {
+    constructor (power) {
+        this._gas = 25;
+        this._power = power;
+    }
+    get power () {
+        return `${this._power}hp`;
+    }
+    get gas () {
+        return `${this._gas}L (${this._gas / 50 * 100}%)`;
+    }
+    set gas (value) {
+        if (value > 50) {
+            value = 50;
+        }
+        else if (value < 0) {
+            value = 0;
+        }
+        this.gas = value;
+    }
+}
+
+let car = new Car (400);
+
+console.log(car.power);
+console.log(car.gas); */
+
+/////////////////// END OF GETTERS AND SETTERS LESSON //////////////////
+
+
+///////////////////// OBJECTS AS ARGUMENTS LESSON ///////////////////////
+
+/* class Car {
+    constructor (model, year, color) {
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+}
+
+const car1 = new Car ("Mustang", 2023, "red");
+const car2 = new Car ("Corvette", 2024, "blue");
+const car3 = new Car ("Lambo", 2022, "yellow");
+
+changeColor(car3, "gold");
+displayInfo(car3)
+
+function displayInfo(car) {
+    console.log(car.model);
+    console.log(car.year);
+    console.log(car.color);
+}
+
+function changeColor(car, color) {
+    car.color = color;
+} */
+
+/////////////////// END OF OBJECTS AS ARGUMENTS LESSON //////////////////
